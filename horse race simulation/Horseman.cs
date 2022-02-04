14483691e00b45827a0c449db65a50993d79f839
MystriLL <74019350+MystriLL@users.noreply.github.com>
2022-02-04 16:45:25 +0100

    feat: Add Horseman class

---
+++ horse race simulation/Horseman.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace horse_race_simulation
+{
+
+    class Horseman
+    {
+        public readonly string Name;
+        public readonly int Skill; // additional factor that affects horse's performance in the race
+        public readonly Horse Horse;
+
+        public Horseman(string name, int skill, Horse horse)
+        {
+            Name = name;
+            Skill = skill;
+            Horse = horse;
+        }
+    }
+
+
+    class Horse
+    {
+        public readonly int Id;
+        public readonly string Name;
+        public readonly int Stamina; //defines energy loss rate
+        public int Speed; 
+        public int Energy = 100; // when falls to 0, speed is decreased by 50% for a while
+
+
+        public Horse(int id, int stamina, int speed)
+        {
+            Id = id;
+            Stamina = stamina;
+            Speed = speed;
+        }
+    }
+   
+}
