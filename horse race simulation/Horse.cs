5949dad9a7358d900efc63883603a73f6cde61e3
MystriLL <74019350+MystriLL@users.noreply.github.com>
2022-02-04 14:54:08 +0100

    feat: implement basic class structure

--- horse race simulation/Horse.cs
+++ horse race simulation/Horse.cs
@@ -6,5 +6,20 @@ namespace horse_race_simulation
 {
     class Horse
     {
+        public readonly int Id;
+        public readonly string Name;
+        public readonly int Stamina; //defines energy loss rate
+        public int Speed; 
+        public int Energy = 100; // when falls to 0, speed is decreased by 50% for a while
+
+
+        public Horse(int id, string name, int stamina, int speed)
+        {
+            Id = id;
+            Name = name;
+            Stamina = stamina;
+            Speed = speed;
+        }
     }
+   
 }
