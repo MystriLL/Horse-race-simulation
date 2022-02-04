1f146c7e3449e792817a070f8a890fc551045d61
MystriLL <74019350+MystriLL@users.noreply.github.com>
2022-02-04 13:37:37 +0100

    Initial commit

---
+++ horse race simulation/Race.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace horse_race_simulation
+{
+    class Race
+    {
+        public void Start()
+        {
+            WriteLine("Race is about to start");
+
+
+
+            WriteLine("\n\nPress any key to exit");
+        }
+    }
+}
---
+++ horse race simulation/Track.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace horse_race_simulation
+{
+    class Track
+    {
+    }
+}
