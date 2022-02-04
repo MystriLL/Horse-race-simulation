bd288b98ecdb1816fb2666bd308458dc7c3936c5
MystriLL <74019350+MystriLL@users.noreply.github.com>
2022-02-04 17:04:52 +0100

    feat: Create list of participants with random stats

--- horse race simulation/Program.cs
+++ horse race simulation/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
-
+using static System.Console;
+using System.Threading;
+using System.Collections.Generic;
 namespace horse_race_simulation
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("Welcome to horse race simulator! \n\n");
+            Thread.Sleep(1500);
+            WriteLine("Specify number participants between 2 and 5: ");
+            int participantsNumber = Convert.ToInt32(ReadLine());
+            
+            List<Horseman> Participants = CreateParticipants(participantsNumber);
+            foreach(var p in Participants)
+            {
+                Thread.Sleep(500);
+                WriteLine($"Hi! My name is {p.Name}, My skill level is {p.Skill}. My horse has {p.Horse.Speed} speed and {p.Horse.Stamina} stamina");
+            }
+        }
+
+        public static List<Horseman> CreateParticipants(int n)
+        {
+            Random rnd = new Random();
+            List<Horseman> participants = new List<Horseman>();
+            List<Horse> horses = new List<Horse>();
+
+            for (int i = 0; i<n; i++)
+            { 
+                horses.Add(new Horse(i, rnd.Next(1,11), rnd.Next(1,11)));
+                participants.Add(new Horseman("Rider " + (i+1), rnd.Next(1, 11), horses[i]));
+            }
+            return participants;
         }
     }
 }
