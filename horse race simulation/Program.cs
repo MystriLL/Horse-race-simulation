c51ef24f1c933a1cf39ff1e42aac1d3d534f1bb4
MystriLL <74019350+MystriLL@users.noreply.github.com>
2022-02-04 13:27:33 +0100

    Dodaj pliki projektów.

---
+++ horse race simulation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace horse_race_simulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
