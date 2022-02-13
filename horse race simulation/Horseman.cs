5c59f9c3de041e9930bf5016dbc325bef8ba6044
MystriLL <74019350+MystriLL@users.noreply.github.com>
2022-02-13 18:23:38 +0100

    feat: add basic move event handling

--- horse race simulation/Horseman.cs
+++ horse race simulation/Horseman.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace horse_race_simulation
 {
 
     class Horseman
     {
+        private static Random rnd = new Random();
         public readonly string Name;
         public readonly int Skill; // additional factor that affects horse's performance in the race
         public readonly Horse Horse;
+        private bool isRunning;
+        public int DistanceTravelled = 0;
+        public event EventHandler<MoveEventArgs> Move;
 
         public Horseman(string name, int skill, Horse horse)
         {
@@ -17,16 +22,29 @@ namespace horse_race_simulation
             Skill = skill;
             Horse = horse;
         }
+
+        public void Run(object sender, EventArgs e)
+        {
+            Console.WriteLine("Horseman ready to race");
+            isRunning = true;
+            while(isRunning && DistanceTravelled < 1000)
+            {
+                Thread.Sleep(1000);
+                int distThisTick = rnd.Next(1, this.Horse.Speed);
+                Move(this, new MoveEventArgs(distThisTick));
+                DistanceTravelled += distThisTick;
+                // to do: implement energy logic
+            }
+        }
     }
 
 
     class Horse
     {
         public readonly int Id;
-        public readonly string Name;
         public readonly int Stamina; //defines energy loss rate
         public int Speed; 
-        public int Energy = 100; // when falls to 0, speed is decreased by 50% for a while
+        public int Energy = 100; // when falls to 0, speed is decreased by half for a while
 
 
         public Horse(int id, int stamina, int speed)
--- horse race simulation/Race.cs
+++ horse race simulation/Race.cs
@@ -1,19 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using static System.Console;
 
 namespace horse_race_simulation
 {
+
+    class MoveEventArgs: EventArgs
+    {
+        public readonly int move;
+        public MoveEventArgs(int m) => move = m;
+    }
+
     class Race
     {
-        public void Start()
+        public int distance = 1000;
+        private EventHandler run = null;
+        private Dictionary<string,int> position = new Dictionary<string, int>();
+
+        public event EventHandler Run
+        {
+            add
+            {
+                run += value;
+                var rider = value.Target as Horseman;
+                position[rider.Name] = 0;
+                rider.Move += this.Move;
+            }
+            remove
+            {
+                run -= value;
+            }
+        }
+
+        public void Start(List<Horseman> participants)
         {
             WriteLine("Race is about to start");
-
-
-
-            WriteLine("\n\nPress any key to exit");
+            foreach (var p in participants)
+                this.Run += p.Run;
+            Thread.Sleep(1000);
+            WriteLine("The race has started!");
+            Task[] riders = new Task[run.GetInvocationList().Length];
+            int i = 0;
+            foreach(EventHandler rider in run.GetInvocationList())
+                riders[i++] = Task.Run(() => rider(this, EventArgs.Empty));
+            Task.WaitAll(riders);
         }
+
+
+        public void Move(object sender, EventArgs e)
+        {
+            var rider = sender as Horseman;
+            position[rider.Name] = rider.DistanceTravelled;  // Updating current position of Horseman
+            if (position[rider.Name] < distance)
+                WriteLine($"Horseman {rider.Name} has travelled {position[rider.Name]} metres so far");
+            else
+            {
+                WriteLine($"Horseman {rider.Name} has finished the race");
+            }
+
+        }
+
+
     }
 }
